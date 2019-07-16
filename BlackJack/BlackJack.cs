0a0bcb94514fd51fa1942798e9c8ef2d39f0016b
Thomas Zhang <thomaszhang@berkeley.edu>
2019-07-16 14:50:24 -0700

    Added basic functionality'

--- BlackJack/BlackJack.cs
+++ BlackJack/BlackJack.cs
@@ -13,10 +13,43 @@ namespace BlackJack
             var game = new Game(new Deck());
             while (true)
             {
-                Console.WriteLine("Welcome to the game.");
-                Console.WriteLine("Your cards are: " + game.playerCards.ToString());
+                Console.WriteLine("Hello");
+                string cards = string.Empty;
+                foreach (Card card in game.playerCards)
+                {
+                    cards += "[" + card.ToString() + "] ";
+                }
+                Console.WriteLine("Your cards are: " + cards);
+                Console.WriteLine("Select either 'hit' or 'stand'");
                 var input = Console.ReadLine();
+                if (input == "hit")
+                {
+                    bool result = game.Hit(game.playerCards);
+                    if (!result)
+                    {
+                        Console.WriteLine("You busted, and thus you suck. GG");
+                        break;
+                    }
+                }
+                else if (input == "stand")
+                {
+                    bool result = game.Stand();
+                    if (result)
+                    {
+                        Console.WriteLine("You actually won. Wow, I guess you're not bad.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Well, the dealer busted. GG");
+                        break;
+                    }
+                }
             }
+            while (true)
+            {
+                System.Console.ReadKey();
+            };
         }
     }
 }
